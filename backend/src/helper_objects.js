62bfd15b6c89e070fedf32e62a63725a8f5241dc
Paseka100 <Pasekamogot@gmail.com>
2025-05-16 19:59:27 +0200

    merge conflict

--- backend/src/helper_objects.js
+++ backend/src/helper_objects.js
@@ -1,38 +1,28 @@
 const queries = {
-  createTable: `
-    CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(250),
-      score INTEGER
-    );
-  `,
-
   createCategoriesTable: `
-    CREATE TABLE IF NOT EXISTS categories (
-      id SERIAL PRIMARY KEY,
-      name TEXT NOT NULL
-    );
-  `,
+   CREATE TABLE IF NOT EXISTS categories (
+     id SERIAL PRIMARY KEY,
+     name TEXT NOT NULL
+   );
+ `,
 
   createQuestionsTable: `
-    CREATE TABLE IF NOT EXISTS questions (
-      id SERIAL PRIMARY KEY,
-      category_id INTEGER NOT NULL REFERENCES categories(id),
-      questions TEXT NOT NULL
-    );
-  `,
+   CREATE TABLE IF NOT EXISTS questions (
+     id SERIAL PRIMARY KEY,
+     category_id INTEGER NOT NULL REFERENCES categories(id),
+     questions TEXT NOT NULL
+   );
+ `,
 
   createAnswersTable: `
-    CREATE TABLE IF NOT EXISTS answers (
-      id SERIAL PRIMARY KEY,
-      question_id INTEGER NOT NULL REFERENCES questions(id),
-      answer TEXT NOT NULL
-    );
-  `,
-
+   CREATE TABLE IF NOT EXISTS answers (
+     id SERIAL PRIMARY KEY,
+     question_id INTEGER NOT NULL REFERENCES questions(id),
+     answer TEXT NOT NULL
+   );
+ `,
   addUserToDatabase: `
-    INSERT INTO users (name) VALUES ($1);
-  `
+ INSERT INTO users (name) VALUES ($1);
+`,
 };
-
 module.exports = { queries };
