1dcf3b1f68b5d9ee9bd3fc457f13a17b3cbb1633
Zaid <zaidakhalwaya6@gmail.com>
2025-05-16 20:44:10 +0200

    added questions for question table

--- backend/src/helper_objects.js
+++ backend/src/helper_objects.js
@@ -22,6 +22,31 @@ const queries = {
     );
   `,
 
+    insertQuestions: `
+    INSERT INTO questions (id, category_id, question)
+    VALUES
+      (1, 1, 'What does stand for CPU?'),
+      (2, 1, 'What does a power supply unit (PSU) do?'),
+      (3, 1, 'GPU stands for?'),
+      (4, 1, 'What does RAM stand for?')
+
+      (5, 2, ' What does an operating system do?'),
+      (6, 2, 'Which of these is an operating system?'),
+      (7, 2, 'What type of software is MS Excel?'),
+      (8, 2, 'What is the purpose of antivirus software')
+
+      (9,  3, 'What does HTML stand for?'),
+      (10, 3, 'Which data type is NOT in JavaScript?'),
+      (11, 3, 'What does JS stand for?'),
+      (12, 3, 'What does CSS stand for?')
+
+      (12, 4, 'What does IP stand for?'),
+      (13, 4, 'Which device directs data between networks?'),
+      (14, 4, 'What is secure web protocol?'),
+      (15, 4, 'HTTP port?'),
+      
+  `,
+
   addUserToDatabase: `
     INSERT INTO users (name) VALUES ($1);
   `
