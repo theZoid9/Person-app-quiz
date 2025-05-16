aae51232c0d8ba3d196b380676764550c6f08a7a
Paseka100 <Pasekamogot@gmail.com>
2025-05-16 18:45:24 +0200

    clean up

--- backend/src/sql_node.js
+++ backend/src/sql_node.js
@@ -1,41 +1,38 @@
-const { pool } = require("./config/config")
-const { queries } = require("./helper_objects")
+const { pool } = require("./config/config");
+const { queries } = require("./helper_objects");
 
 async function createTable() {
-    await pool.query(queries.createTable, (error) => {
-      if (error) {
-        throw new Error(error.message);
-      }
-    });
-    return "table users succefully created";
-  }
-  async function createQuestionTables(){
-    await pool.query(queries.createQuestionsTable,(error)=>{
-        if (error) {
-            throw new Error(error.message);
-          }
-          return "table questions succefully created";
-git
-    })
-
-  }
-  createTable().then(result => {
-    console.log(result);
-  })
-  createQuestionTables().then(message=>{
-    console.log(message)
-  })
-
-  async function addUser(name) {
-    try {
-      const result = await pool.query(queries.addUserToDatabase, [name]);
-      return `User '${name}' successfully added to the database`;
-    } catch (error) {
-      throw new Error("Error adding user: " + error.message);
+  await pool.query(queries.createTable, (error) => {
+    if (error) {
+      throw new Error(error.message);
     }
+  });
+  return "table users succefully created";
+}
+async function createQuestionTables() {
+  await pool.query(queries.createQuestionsTable, (error) => {
+    if (error) {
+      throw new Error(error.message);
+    }
+    return "table questions succefully created";
+  });
+}
+createTable().then((result) => {
+  console.log(result);
+});
+createQuestionTables().then((message) => {
+  console.log(message);
+});
+
+async function addUser(name) {
+  try {
+    const result = await pool.query(queries.addUserToDatabase, [name]);
+    return `User '${name}' successfully added to the database`;
+  } catch (error) {
+    throw new Error("Error adding user: " + error.message);
   }
-  
-  addUser('paseka mogoto').then((message) => {
-    console.log(message);
-  })
-  
+}
+
+/*addUser("paseka mogoto").then((message) => {
+  console.log(message);
+});*/
