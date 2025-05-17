1b7f17ddb01d7dca88a54fe62de77c48cc72342e
Aaron Jiane <devmfundo@gmail.com>
2025-05-18 01:16:20 +0200

    added cors dependency for allowing api requests

--- frontend/src/js/homepage.js
+++ frontend/src/js/homepage.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       try {
-        const response = await fetch("http://localhost:3000/api/leaderboard",{
+        const response = await fetch("https://refreshing-vibrancy-production.up.railway.app/api/leaderboard",{
           method: "POST",
           headers: {
             "Content-Type": "application/json"
