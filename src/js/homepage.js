b32f68b37f02f1c81310bcabba4f0b40cb302b57
Zaid <zaidakhalwaya6@gmail.com>
2025-05-18 16:59:53 +0200

    fix getHighscore

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,25 +1,14 @@
+const {
+  getHighscores
+} = require('../services/api');
 
 document.getElementById("getStartedBtn").addEventListener("click", () => {
-    const username = document.getElementById("username").value.trim();
+  const username = document.getElementById("username").value.trim();
 
-    if (!username) {
-      alert("Please enter a name.");
-      return;
-    }
+  if (!username) {
+    alert("Please enter your name!");
+    return;
+  }
 
-    fetch("person-app-quiz-production.up.railway.app/api/highscores", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ name: username ,"score": 0 })
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log("Success:", data);
-     
-    })
-    .catch(error => {
-      console.error("Error:", error);
-    });
-  });
+  getHighscores(username);
+});
