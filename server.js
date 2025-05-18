272af26d3fdb105c08cd5cd5f9305ab88d088506
Zaid <zaidakhalwaya6@gmail.com>
2025-05-18 19:37:07 +0200

    add cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "src")));
