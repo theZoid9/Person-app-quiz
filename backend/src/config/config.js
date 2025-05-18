f4b926b79e13dc6a2d5ab7239b22a4c62aa578d6
Zaid <zaidakhalwaya6@gmail.com>
2025-05-18 04:23:46 +0200

    test postgres prod

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,10 +1,15 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 require("dotenv").config();
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL environment variable.");
+}
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || `postgresql://postgres:pass@localhost:5432/quiz`,
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
+
 pool.on("connect", () => {
   console.log("Connected to PostgreSQL database");
 });
