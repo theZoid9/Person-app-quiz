9afab355757a04f14f9b10f0cae3380234969010
Zaid <zaidakhalwaya6@gmail.com>
2025-05-18 22:32:08 +0200

    Get all options with ID

---
+++ src/js/quiz.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const answers = JSON.parse(fs.readFileSync('src/data/answer.json', 'utf8'));
+
+function getOptionById(id) {
+  
+  const question = answers.find(q => q.question_id === id);
+  return question ? question.options : [];
+}
+  
+
+console.log(getOptionById(1));
