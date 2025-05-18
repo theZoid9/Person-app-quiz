8d66dae3933f3457adbc0d207f4a700a46efdbb6
Aaron Jiane <devmfundo@gmail.com>
2025-05-18 16:04:54 +0200

    addHighScores testing

--- src/services/api.js
+++ src/services/api.js
@@ -1,32 +1,29 @@
 const getAnswers=async()=>{
-  const res = await fetch("https://person-app-quiz-production.up.railway.app/api/answers");
+  const res = await fetch("/api/answers");
   return await res.json();
 }
 
 const getCategory=async()=>{
-  const res = await fetch("https://person-app-quiz-production.up.railway.app/api/category");
+  const res = await fetch("/api/category");
   return await res.json();
 }
 
 const getHighscores=async()=>{
-  const res = await fetch("https://person-app-quiz-production.up.railway.app/api/highscores");
+  const res = await fetch("/api/highscores");
   return await res.json();
 }
 
 
 const addHighscores=async(user)=>{
-  const res = await fetch("https://person-app-quiz-production.up.railway.app/api/highscores", {
+  const res = await fetch("/api/highscores", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user)
-  })
+  });
+  console.log(res.json())
   return await res.json();
 }
-const user = {name:"Khanyiso", score: 20}
-const user1 = {name:"Zaid", score: 14}
-const user2 = {name:"Paseka", score: 16}
+const user = {name:"Aaron", score: 10}
 addHighscores(user)
-addHighscores(user1)
-addHighscores(user2)
 
 module.exports={getAnswers,getCategory,addHighscores,getHighscores}
