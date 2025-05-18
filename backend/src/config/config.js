fa93e02878ea9122130246eff66ba846c733be00
Zaid <zaidakhalwaya6@gmail.com>
2025-05-18 05:07:46 +0200

    Connect to Railway Postgres trying modifying pool

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,22 +1,5 @@
-require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, 
-  },
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
-async function test() {
-  try {
-    const res = await pool.query('SELECT NOW()');
-    console.log('Connected! Time:', res.rows[0]);
-  } catch (err) {
-    console.error('Connection error:', err);
-  } finally {
-    await pool.end();
-  }
-}
-
-test();
+module.exports = { pool };
