786db79322ffaf08e65772291eaaa1641b17e48e
Zaid <zaidakhalwaya6@gmail.com>
2025-05-18 22:35:42 +0200

    fix

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -3,10 +3,9 @@ const fs = require('fs');
 const answers = JSON.parse(fs.readFileSync('src/data/answer.json', 'utf8'));
 
 function getOptionById(id) {
-  
+ 
   const question = answers.find(q => q.question_id === id);
   return question ? question.options : [];
 }
-  
 
 console.log(getOptionById(1));
